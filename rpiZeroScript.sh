9b97432f71d367e82495837367734b18d82a9cc8
Victor Delaplaine <vdelaplainess@gmail.com>
2018-12-20 16:57:40 -0800

    Update rpiZeroScript.sh

--- rpiZeroScript.sh
+++ rpiZeroScript.sh
@@ -1,1 +1,16 @@
+#!/bin/sh
+date +'FORMAT'
+ 
+### mm/dd/yyyy ###
+date +'%m/%d/%Y'
+ 
+## Time in 12 hr format ###
+date +'%r'
+ 
+## backup dir format ##
+backup_dir=$(date +'%m-%d-%Y')
+echo "Backup dir for today: /var/www/html/Camera/${backup_dir}"
 
+rsync -av -e 'ssh -i ~/.ssh/camera-key.pem' /var/www/html/media/* ubuntu@ec2-18-217-65-68.us-east-2.compute.amazonaws.com:/var/www/html/Camera/${backup_dir}/
+
+sudo rm -f /var/www/html/media/*
